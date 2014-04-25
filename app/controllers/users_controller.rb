2d87e6ed948e4801fd596e7ff6db0bb2ef36d7c1
siri innovations <siri@siri-To-be-filled-by-O-E-M.(none)>
2014-04-25 16:01:05 +0530

    recent

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,20 @@
 class UsersController < ApplicationController
    before_filter :authenticate_admin!
   
-  # def index #for public videos
-  # 	@user_info = Vimeo::Simple::User.videos("user25247730") 
-  # end
-
-
-  def index #for private videos
-	  	@base = Vimeo::Advanced::Base.new("48315613d6a6df52a740c1b44038453a5f61300e", "5daf0e2a00cdd9df59bb6e58ccca4e344ddc03da")
-	  	puts "#{@base.inspect}//////////////////////////////"
-	  	request_token = @base.get_request_token
-	  	puts "#{request_token}////////////////////////////////"
-	  	session[:oauth_secret] = request_token.secret
-	  	redirect_to @base.authorize_url
+  def index #for public videos
+  	#@user_info = Vimeo::Simple::User.videos("user25247730") 
+    @user_info = Vimeo::Simple::User.videos("user9436893") 
   end
+
+
+  # def index #for private videos
+	 #  	@base = Vimeo::Advanced::Base.new("6e5c231d7fe43552b47b7dbd2971a795d5bbc3ca", "7f54ccb85f39876529230420e73c34ac75a63c9d")
+	 #  	puts "#{@base.inspect}//////////////////////////////"
+	 #  	request_token = @base.get_request_token
+	 #  	puts "#{request_token}////////////////////////////////"
+	 #  	session[:oauth_secret] = request_token.secret
+	 #  	redirect_to @base.authorize_url
+  # end
 
   def new
   end
@@ -22,7 +23,7 @@ class UsersController < ApplicationController
   end
 
   def get_details
-    base = Vimeo::Advanced::Base.new("48315613d6a6df52a740c1b44038453a5f61300e", "5daf0e2a00cdd9df59bb6e58ccca4e344ddc03da")
+    base = Vimeo::Advanced::Base.new("6e5c231d7fe43552b47b7dbd2971a795d5bbc3ca", "7f54ccb85f39876529230420e73c34ac75a63c9d")
     access_token = base.get_access_token(params[:oauth_token], session[:oauth_secret], params[:oauth_verifier])
     @user_token = access_token.token
     @user_secret = access_token.secret
